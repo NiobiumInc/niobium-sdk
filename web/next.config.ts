aa5cd81a1a6232083913497c4e4a75a1b7218ab9
Brian Shirai <brian@niobiummicrosystems.com>
2024-12-06 09:53:46 -0800

    Fix path prefix for static site gen.
    Docs are wrong, when building for GitHub Pages with a custom domain
    configured, do not use the repo name as path prefix because GitHub
    serves the content differently than when serving at
    <username>.github.io.

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'out',
-  basePath: '/niobium-sdk',
-  assetPrefix: '/niobium-sdk/',
+  basePath: '',
+  assetPrefix: '/',
   trailingSlash: true,
 };
 
