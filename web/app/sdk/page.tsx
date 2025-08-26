82a3b07da3bad1b40555d7c6553dc07f8c163d8a
Brian Shirai <brian@niobiummicrosystems.com>
2025-08-26 13:53:33 -0700

    Add SDK links.

--- web/app/sdk/page.tsx
+++ web/app/sdk/page.tsx
@@ -1,10 +1,26 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
     <div>
       <h1 className="text-3xl font-bold">Niobium Developer SDK</h1>
       <p>
-        This is how you get the SDK.
+        The Niobium FHE SDK is packaged as a Docker container as part of the
+        Niobium client/server runtime system. Please visit the following links
+        to learn more:
       </p>
+      <ul>
+      <li>
+        <Link href="/articles/understanding-niobium-fhe-sdk/">
+          Understanding the Niobium FHE Software Development Kit
+        </Link>
+      </li>
+      <li>
+        <Link href="/tutorials/getting-started-with-niobium-client-server-rts/">
+          Getting Started with the Niobium Client/Server RTS
+        </Link>
+      </li>
+      </ul>
     </div>
   );
 }
