62b9409a238f754f637e5868bead1dccfd089212
Brian Shirai <brian@niobiummicrosystems.com>
2025-08-26 14:11:49 -0700

    Add other menu page links.

--- web/app/tutorials/page.tsx
+++ web/app/tutorials/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
     <div>
@@ -5,6 +7,13 @@ export default function Page() {
       <p>
         A list of all the tutorials.
       </p>
+      <ul>
+      <li>
+        <Link href="/tutorials/getting-started-with-niobium-client-server-rts/">
+          Getting Started with the Niobium Client/Server RTS
+        </Link>
+      </li>
+      </ul>
     </div>
   );
 }
